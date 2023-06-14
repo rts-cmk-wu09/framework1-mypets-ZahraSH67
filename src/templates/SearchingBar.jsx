bfb0cb8ec4c6faf38ffbc685cf851e4f51610e02
Zahra Shahabi <zahra.shahabi.dk@gmail.com>
2023-06-14 20:06:56 +0200

    add searchign bar to project

--- src/templates/SearchingBar.jsx
+++ src/templates/SearchingBar.jsx
@@ -3,22 +3,34 @@ import LocationBox from "./LocationBox";
 import Image from "../components/Image";
 import CircleButton from "../components/CircleButton";
 import Img from "../assets/Profile.png"
-
+import {FiBell} from "react-icons/fi";
 
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: row;
+    align-items: center;
+`
+const StyledSearchingBar = styled.div`
+    display: flex;
+    flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    margin-top: 40px;
+    // border: 1px solid red;
+    padding: 0 20px;
 `
 
 const SearchingBar = () => {
     return ( 
-        <StyledDiv>
-            <Image src={Img} width="40" height="40" border-radius="44"/>
-            <LocationBox />
-            <CircleButton />
-        </StyledDiv>
+
+        <StyledSearchingBar>
+            <StyledDiv>
+                <Image src={Img} width="40" height="40" radius="44"/>
+                <LocationBox margin="60"/>
+            </StyledDiv>
+            <CircleButton picture={<FiBell color="5533EA" />} />
+        </StyledSearchingBar>
+        
      );
 }
  
